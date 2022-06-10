8d8f34335462d2cd50349f77bcea30d285b7430b
mandara11 <mandaram2345@gmail.com>
2022-06-10 16:27:34 +0530

    Add project files.

---
+++ D27AddressBook/Address.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace D27AddressBook
+{
+    public class Address
+    {
+        public string FirstName;
+        public string LastName;
+        public string City;
+        public string State;
+        public string ZipCode;
+        public string PhoneNumber;
+        public string Email;
+
+        public static List<Address> All = new List<Address>();
+
+        private static void AddAddress()
+        {
+            Address address = new Address();
+
+            Console.WriteLine("Enter First Name:");
+            address.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name:");
+            address.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter city: ");
+            address.City = Console.ReadLine();
+
+            Console.WriteLine("Enter the State:");
+            address.State = Console.ReadLine();
+
+            Console.WriteLine("Enter the Zipcode:");
+            address.ZipCode = Console.ReadLine();
+
+            Console.WriteLine("Enter the PhoneNumber:");
+            address.PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Enter the Email:");
+            address.Email = Console.ReadLine();
+
+        }
+    }
+}
---
+++ D27AddressBook/CreateContact.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D27AddressBook
+{
+    public class CreateContact //UC1
+    {
+
+        public List<Contact> ContactBook = new List<Contact>();
+        public void createContactMethod()
+        {
+            Contact obj1 = new Contact();
+            obj1.FirstName = "Mandara";
+            obj1.LastName = "M";
+            obj1.City = "Banglore";
+            obj1.State = "Karnataka";
+            obj1.ZipCode = "560035";
+            obj1.PhoneNumber = "9110264684";
+            obj1.E_Mail = "mandaram2345@gmail.com";
+            ContactBook.Add(obj1);
+            Console.WriteLine("\nADDRESS BOOK Is Created ");
+        }
+
+        //Printing ContactBook
+        public void printContact()
+        {
+            Console.WriteLine("\nPrinting Contact information ..................................\n");
+            foreach (var obje in ContactBook)
+            {
+                Console.WriteLine("FirstName is "+ obje.FirstName);
+                Console.WriteLine("LastName is "+ obje.LastName);
+                Console.WriteLine("City is "+ obje.City);
+                Console.WriteLine("State is "+ obje.State);
+                Console.WriteLine("ZipCode is "+ obje.ZipCode);
+                Console.WriteLine("PhoneNumber is "+ obje.PhoneNumber);
+                Console.WriteLine("E_Mail is "+ obje.E_Mail);
+            }
+        }
+    }
+}
